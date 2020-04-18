469ee914029e1fb145da3bf9bb9f3bcab80d1b1e
Karan Mhatre <me@karanmhatre.com>
2020-04-18 20:18:37 +0530

    animation added

--- js/builder.js
+++ js/builder.js
@@ -1,7 +1,21 @@
 paper.install(window);
 
+function getPosition() {
+  var canvas = document.getElementById("myCanvas");
+
+  var xBound = window.innerWidth;
+  var yBound = window.innerHeight;
+
+  x = Math.floor(Math.random() * (xBound-100));
+  y = Math.floor(Math.random() * (yBound));
+
+  point = new Point(x, y);
+
+  return point;
+}
+
 function getRandomNumber(min=0, max) {
-  return Math.floor(Math.random() * (min-max)) + min;
+  return Math.floor(Math.random() * (max-min)) + min;
 }
 
 function getColor() {
@@ -9,7 +23,9 @@ function getColor() {
   return colorList[Math.floor(Math.random() * colorList.length)];
 }
 
-function getSize(min, max) {
+function getSize() {
+  min = 100
+  max = window.innerWidth/2
   return Math.floor(Math.random() * max)+min;
 }
 
@@ -26,17 +42,20 @@ function fillBackground(colour, canvas) {
 
   path.fillColor = colour;
   path.strokeColor = colour;
+
+  path.sendToBack()
   // path.selected = true;
 }
 
-function makeSemiCircle(firstPoint) {
+function makeSemiCircle() {
+
+  firstPoint = getPosition();
 
   var myPath = new Path();
   myPath.fillColor = getColor();
   myPath.opacity = getOpacity();
 
-  sizeWidth = window.innerWidth/2;  
-  size = getSize(100,sizeWidth);
+  size = 400;
 
   secondPoint = new Point(firstPoint.x+size, firstPoint.y);
 
@@ -47,102 +66,65 @@ function makeSemiCircle(firstPoint) {
   myPath.insert(3, new Point(firstPoint.x + (size - (size/6)), firstPoint.y-300));
   myPath.closed = true;
 
-  myPath.smooth({
-    type: 'catmull-rom',
-    factor: 0.2,
-    from: -1,
-    to: 1
-  });
-
-  myPath.smooth({
-   type: 'catmull-rom',
-   factor: 0.2,
-   from: 0,
-   to: 4
-  });
+  myPath.smooth({type: 'catmull-rom', factor: 0.2, from: -1, to: 1 });
+  myPath.smooth({ type: 'catmull-rom', factor: 0.2, from: 0, to: 4 });
 
   angles = [90,180,-90,-180];
   myPath.rotate(angles[Math.floor(Math.random()*angles.length)]);
 }
 
-function makeCircleScribble(firstPoint) {
-  var center = firstPoint;
+function makeCircleScribble() {
+  var center = getPosition();
   var sides = 4;
 
-  sizeWidth = window.innerWidth/2;  
-  var radius = getSize(100,sizeWidth);
+  var radius = getSize();
 
   var polygon = new Path.RegularPolygon(center, sides, radius);
   polygon.strokeColor = getColor();
   polygon.strokeWidth = 2;
   polygon.opacity = getOpacity();
 
-  polygon.smooth({
-    type: 'continuous',
-   // factor: 1,
-  });
+  polygon.smooth({ type: 'continuous'});
 
-  polygon.smooth({
-    type: 'catmull-rom',
-    factor: 0.1,
-    from: 1,
-    to: 3
-  });
+  polygon.smooth({type: 'catmull-rom',factor: 0.1,from: 1, to: 3 });
 
-  polygon.smooth({
-    type: 'catmull-rom',
-    factor: 0.1,
-    from: 0,
-    to: 1
-  });
+  polygon.smooth({type: 'catmull-rom', factor: 0.1, from: 0, to: 1 });
 
   angles = [90,180,-90,-180];
   polygon.rotate(angles[Math.floor(Math.random()*angles.length)]);
 }
 
-function makeCircle(firstPoint) {
-  var center = firstPoint;
+function makeCircle() {
+  var center = getPosition();
   var sides = 4;
 
-  sizeWidth = window.innerWidth/2;  
-  var radius = getSize(100,sizeWidth);
+  var radius = 200;
 
   var polygon = new Path.RegularPolygon(center, sides, radius);
   polygon.fillColor = getColor();
   polygon.opacity = getOpacity();
 
-  polygon.smooth({
-    type: 'continuous',
-   // factor: 1,
-  });
-
-  polygon.smooth({
-    type: 'catmull-rom',
-    factor: 0.1,
-    from: 1,
-    to: 3
-  });
-
-  polygon.smooth({
-    type: 'catmull-rom',
-    factor: 0.1,
-    from: 0,
-    to: 1
-  });
+  polygon.smooth({ type: 'continuous' });
+  polygon.smooth({ type: 'catmull-rom', factor: .5, from: 1, to: 3 });
+  polygon.smooth({ type: 'catmull-rom', factor: .5, from: 0, to: 1 });
 
   angles = [90,180,-90,-180];
   polygon.rotate(angles[Math.floor(Math.random()*angles.length)]);
 }
 
-function makeLinesPattern(firstPoint) {
+function makeLinesPattern() {
+
+  firstPoint = getPosition();
 
   var myPath = [];
   sizeWidth = window.innerWidth/2;
-  var size = getSize(100,sizeWidth);
+  var size = getSize();
 
   noOfLines = (Math.random()*10)+1;
 
   color = getColor();
+
+  var linesPatternGroup = new Group();
 
 
   for(var i=0;i<noOfLines;i++) {
@@ -153,9 +135,6 @@ function makeLinesPattern(firstPoint) {
     x1 = firstPoint.x+(i*8)+(Math.random()*5)-5;
     y1 = firstPoint.y+(Math.random()*5)-5;
     
-    // x2 = firstPoint.x+(i*8);
-    // y2 = firstPoint.y+size;
-    
     myPath[i].add(x1, y1);
 
     for(var j=1;j<11;j++) {
@@ -165,48 +144,61 @@ function makeLinesPattern(firstPoint) {
     }
 
     myPath[i].smooth();
+
+    linesPatternGroup.addChild(myPath[i]);
   }
 
 }
 
 function generateArt() {
 
-  var canvas = document.getElementById("myCanvas");
+  project.clear()
 
-  var xBound = window.innerWidth;
-  var yBound = window.innerHeight;
+  var canvas = document.getElementById("myCanvas");
 
   fillBackground('#F2EEE9', canvas);
 
-  points = [];
+  elementCount = getRandomNumber(1,5);
 
-  elementCount = Math.floor(Math.random() * 5)+1;
-
-
-   for(var i=0; i < elementCount; i++) {
- 
-    x = Math.floor(Math.random() * (xBound-100));
-    y = Math.floor(Math.random() * (yBound));
-
-    point = new Point(x, y);
+  for(var i=0; i < elementCount; i++) {
 
     selectElement = Math.floor(Math.random() * 4);
 
     switch(selectElement) {
      case 0:
-       makeSemiCircle(point);
+       makeSemiCircle();
        break;
      case 1:
-       makeCircle(point);
+       makeCircle();
        break;
      case 2:
-       makeLinesPattern(point);
+       makeLinesPattern();
        break;
 
     case 3:
-      makeCircleScribble(point);
+      makeCircleScribble();
       break;
     }
      
-   }
+  }
+
+  view.draw();
+  // console.log(project.activeLayer.children.length);
+}
+
+function animation() {
+  view.onFrame = function(event) {
+
+    if(Math.floor(event.time) != 0 && Math.floor(event.count)%125 == 0) {
+      generateArt();
+    }
+
+    for(i=1;i<project.activeLayer.children.length;i++) {
+      project.activeLayer.children[i].rotate(1.4*i);
+    }
+  }
+}
+
+function stopAnimation() {
+  view.detach('frame', this.onFrame);
 }
--- js/main.js
+++ js/main.js
@@ -4,11 +4,6 @@ window.onload = function() {
   paper.setup('myCanvas');
 
   var canvas = document.getElementById("myCanvas");
-  // var parent = document.getElementById("container");
-  // 
-  // canvas.width = parent.offsetWidth;
-  // canvas.height = window.innerWidth;
-  // canvas.height = window.innerHeight;
 
   document.getElementById("btn").addEventListener("click", function() {
 
@@ -22,5 +17,35 @@ window.onload = function() {
   document.getElementById("generateBtn").addEventListener("click", function() {
     generateArt();
   });
+
+  document.getElementById("animateBtn").addEventListener("click", function() {
+
+    var controls = document.getElementById("controls");
+
+    var x = document.getElementById("audio");
+    
+    if(this.getAttribute("data-playing") == "1") {
+      stopAnimation();
+      this.setAttribute("data-playing", "0");
+      event.srcElement.innerHTML = "Animate";
+      x.pause();
+    }
+    else {
+      animation();
+      this.setAttribute("data-playing", "1");
+      event.srcElement.innerHTML = "Stop";
+      controls.classList.add("hide");
+      document.getElementById("showControlsContainer").classList.remove("hide");
+      x.play();
+    }
+
+  });
+
+  document.getElementById("showControls").addEventListener("click", function() {
+    controls.classList.remove("hide");
+    document.getElementById("showControlsContainer").classList.add("hide");
+  });
+
+  generateArt();
 
 }
